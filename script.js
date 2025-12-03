98d3abfe2cabd6694f25b1c6f5379eb0bc4969d1
Isethima <216603808+Isethima@users.noreply.github.com>
2025-12-02 20:53:03 -0500

    Add number generation return rock paper and scissors

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hello World")
+//prompt user input rock, paper, or scissors//
+//other input return invalid//
+//generate number 0-2 for computer//
+//assign 0-2 to rock, paper, scissors//
+//if user rock pc rock return draw//
+//if user rock pc scissors return win//
+//if user rock pc paper return lose//
+//if user paper pc rock return win//
+//if user paper pc paper return draw//
+//if user paper pc scissors return lose//
+//if user scissors pc rock return lose//
+//if user scissors pc paper return win//
+//if user scissors pc scissors return draw//
+
+
+function getComputerChoice(){
+    let x = Math.random();
+
+    if ( x <= 1/3){
+        return "rock";
+    } else if ( x >= 2/3){
+        return "paper";
+    } else if ( x <= 2/3 && x >= 1/3){
+        return "scissors";
+    }
+}
+console.log(getComputerChoice())
