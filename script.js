99f0a9fd52a2bfee1508fddbd186cdc0996f02f5
Isethima <216603808+Isethima@users.noreply.github.com>
2025-12-14 22:02:47 -0500

    add outline for playing rounds

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-//prompt user input rock, paper, or scissors//
-//other input return invalid//
-//generate number 0-2 for computer//
-//assign 0-2 to rock, paper, scissors//
-//if user rock pc rock return draw//
-//if user rock pc scissors return win//
-//if user rock pc paper return lose//
-//if user paper pc rock return win//
-//if user paper pc paper return draw//
-//if user paper pc scissors return lose//
-//if user scissors pc rock return lose//
-//if user scissors pc paper return win//
-//if user scissors pc scissors return draw//
 
 
 function getComputerChoice(){
@@ -39,8 +26,18 @@ function getHumanChoice(){
 }
 console.log(getHumanChoice())
 
-var = humanScore
+function playRound(humanChoice, computerChoice){
+    
+}
+    console.log(playRound(humanChoice, computerChoice));
 
-var = computerScore
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+
+
+
 //if computer wins return//
 
